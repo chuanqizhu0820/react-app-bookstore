83cc9f9c3e7bf7763f33869f0e6c780b1bb3382d
Chuanqi Zhu <chuanqizhu1991@outlook.com>
2021-11-15 10:05:55 +0000

    Add feature folder structure

--- src/components/books.js
+++ src/components/books.js
@@ -1,5 +1,40 @@
+const bookArr = [
+    {
+        title: "Some title",
+        author: "Some author",
+        year: "2021"
+    },
+    {
+        title: "Some title",
+        author: "Some author",
+        year: "2020"
+    },
+    {
+        title: "Some title",
+        author: "Some author",
+        year: "2019"
+    }
+]
+
 function Books(){
-    return <h1>Book List</h1>
+    return (
+    <>
+    <h1>Book List</h1>
+    <form>
+        <input type="text" />
+        <button type="button">Add a book</button>
+    </form>
+    <ul>
+        {bookArr.map((item=>{
+            return (
+            <div>
+            <li>{`${item.title}, ${item.author}, ${item.year}`}</li>
+            <button type="button">delete</button>
+            </div>)
+        }))}
+    </ul>
+    </>
+    )
 }
 
 export default Books
