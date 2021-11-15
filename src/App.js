e13008ff9de1c0a895998e620aa81ca8227699a8
Chuanqi Zhu <chuanqizhu1991@outlook.com>
2021-11-15 09:51:38 +0000

    Add two routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
+import Books from "./components/books";
+import Categories from "./components/categories";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+
 function App() {
   return (
-    <div className="App">
-      <h1>Hello, React!</h1>
-    </div>
+      <Router>
+      <div>
+        <Link to="/">Books</Link>
+      </div>
+      <div>
+        <Link to="/categories">Categories</Link>
+      </div>
+
+      <Switch>
+        <Route exact path="/">
+          <Books />
+        </Route>
+        <Route path="/categories">
+          <Categories />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/components/books.js
@@ -0,0 +1,5 @@
+function Books(){
+    return <h1>Book List</h1>
+}
+
+export default Books
