d08c43fd591e316179e529226c9ed0097d0656b3
Chuanqi Zhu <chuanqizhu1991@outlook.com>
2021-11-15 10:39:46 +0000

    Rearrange App component

--- src/index.js
+++ src/index.js
@@ -1,6 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import {
+  BrowserRouter as Router, Switch, Route, Link,
+} from 'react-router-dom';
+import Books from './components/books';
+import Categories from './components/categories';
+import './index.css';
+
+function App() {
+  return (
+    <Router>
+      <div id="navlinks">
+        <div className="nav-link">
+          <Link to="/">Books</Link>
+        </div>
+        <div className="nav-link">
+          <Link to="/categories">Categories</Link>
+        </div>
+      </div>
+
+      <Switch>
+        <Route exact path="/">
+          <Books />
+        </Route>
+        <Route path="/categories">
+          <Categories />
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
 
 ReactDOM.render(
   <React.StrictMode>
